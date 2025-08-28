b729e0022e617153a2d81d1f4ce2b1e0f2f21061
tera1707 <Masa1017>
2025-08-28 23:38:32 +0900

    コード整理

--- MyMcpServer/Program.cs
+++ MyMcpServer/Program.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ModelContextProtocol;
 using ModelContextProtocol.Server;
-using System.Net.Http.Headers;
 
 [McpServerToolType]
 internal class Program

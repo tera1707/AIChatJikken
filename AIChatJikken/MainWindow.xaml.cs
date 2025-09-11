eeaffa346d93f6742e3c080e1def10c513c420dc
tera1707 <Masa1017>
2025-09-11 22:06:35 +0900

    AI同士の討論を実装

--- AIChatJikken/MainWindow.xaml.cs
+++ AIChatJikken/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.ClientModel;
 using System.Collections;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 using Windows.Devices.Usb;
 
 namespace AIChatJikken;
@@ -15,7 +16,10 @@ namespace AIChatJikken;
 public partial class MainWindow : Window
 {
     MyMcpClient? mcpClient;
-    MyChatClient? chatClient;
+    MyChatClient? chatClient1;
+    MyChatClient? chatClient2;
+
+    private static readonly string prerequisites = "以下の内容について、質問をされたことを優先して、コメントをしてください。また、最後はこれまでの会話に関する質問で終わってください。\r\n--------\r\n";
 
     public MainWindow()
     {
@@ -34,27 +38,65 @@ public partial class MainWindow : Window
         var credential = new ApiKeyCredential(configuration["AzureOpenAI:Token"]!);
         var endpoint = new Uri(configuration["AzureOpenAI:Endpoint"]!);
         //chatClient = new MyChatClient(endpoint, credential, await mcpClient?.GetMcpClientTools());
-        chatClient = new MyChatClient(endpoint, credential, null);
+        chatClient1 = new MyChatClient(endpoint, credential, null);
+        chatClient2 = new MyChatClient(endpoint, credential, null);
 
-        PromptBox.Text = "休日のパパはなにをしていますか？";
+        PromptBox.Text = "日本の国内旅行をしたいのですが、どこに行こうか迷っています。北海道から沖縄まで、夏の旅行のお勧めがあれば教えてください。";
     }
 
     private async void Button_Click(object sender, RoutedEventArgs e)
     {
         SendButton.IsEnabled = false;
-        ResponseBlock.Text = "";
-        var prompt = PromptBox.Text;
+        ResponseBlock1.Text = "";
+        ResponseBlock2.Text = "";
+        var userPrompt = PromptBox.Text;
 
-        if (prompt is null)
+        if (userPrompt is null)
             return;
 
-        await chatClient.GetCompletionAsync(prompt, (response =>
+        //まずは1番に、入力した問いを投げかける
+        string prompt0 = prerequisites + userPrompt;
+        GetTargetTextBlock(0)!.Text += $"■No.{(0 % 2) + 1}-----------\r\n";
+        await GetTargetClient(0)!.GetCompletionAsync(prompt0, (response =>
         {
-            ResponseBlock.Text += response;
+            GetTargetTextBlock(0)!.Text += response;
         }));
+        GetTargetTextBlock(0)!.Text += "\r\n-----------------\r\n";
+
+
+        string aitenoResponse = "";
+        aitenoResponse += GetTargetTextBlock(0)!.Text;
+
+        // その後は、お互いのレスポンスを入力として、会話を続ける。
+        for (int i = 1; i < 7; i++)
+        {
+            string prompt = prerequisites + aitenoResponse;
+            aitenoResponse = "";
+
+            GetTargetTextBlock(i)!.Text += $"■No.{(i % 2) + 1}-----------\r\n";
+            await GetTargetClient(i)!.GetCompletionAsync(prompt, (response =>
+            {
+                GetTargetTextBlock(i)!.Text += response;
+                aitenoResponse += response;
+            }));
+            GetTargetTextBlock(i)!.Text += "\r\n-----------------\r\n";
+            await Task.Delay(2000);
+        }
+
 
         PromptBox.Text = "";
         SendButton.IsEnabled = true;
+
+        MyChatClient? GetTargetClient(int count)
+        {
+            return ((count % 2) == 0) ? chatClient1 : chatClient2;
+        }
+
+
+        TextBlock? GetTargetTextBlock(int count)
+        {
+            return ((count % 2) == 0) ? ResponseBlock1 : ResponseBlock1;
+        }
     }
 
     private async void Window_Closed(object sender, EventArgs e)

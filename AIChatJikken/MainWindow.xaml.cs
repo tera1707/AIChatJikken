c76095270767db88a0326aec223ab2009701757b
tera1707 <Masa1017>
2026-02-09 23:24:35 +0900

    キャラ設定、会話回数を設定できるようにした

--- AIChatJikken/MainWindow.xaml.cs
+++ AIChatJikken/MainWindow.xaml.cs
@@ -1,15 +1,6 @@
-﻿using ABI.Windows.ApplicationModel.Activation;
-using Azure;
-using Azure.AI.OpenAI;
-using Microsoft.Extensions.AI;
-using Microsoft.Extensions.Configuration;
-using ModelContextProtocol.Client;
+﻿using Microsoft.Extensions.Configuration;
 using System.ClientModel;
-using System.Collections;
-using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using Windows.Devices.Usb;
 
 namespace AIChatJikken;
 
@@ -25,7 +16,7 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    private async void Window_Loaded(object sender, RoutedEventArgs e)
+    private void Window_Loaded(object sender, RoutedEventArgs e)
     {
         var configuration = new ConfigurationBuilder().AddUserSecrets<MainWindow>().Build();
 
@@ -39,11 +30,6 @@ public partial class MainWindow : Window
         PromptBox.Text = "昨今の環境問題について討論をさせてください。まずごみ問題についてどう思いますか？";
     }
 
-    // 入れたい機能
-    // 2人のキャラ入力
-    // 何回会話させるか
-
-
     private async void Button_Click(object sender, RoutedEventArgs e)
     {
         if (string.IsNullOrEmpty(PromptBox.Text))
@@ -52,53 +38,61 @@ public partial class MainWindow : Window
         if (chatClient1 is null || chatClient2 is null)
             return;
 
+        // 画面で設定したものを取得
+        var userPrompt = PromptBox.Text;
+        int maxCoversation = int.Parse(MaxConversationCount.Text);
+        string[] characterSetting = { Character1Name.Text, Character2Name.Text };
+
         // チャットの出力欄をリセット
         SendButton.IsEnabled = false;
         ResponseBlock1.Text = "";
 
-        var userPrompt = PromptBox.Text;
-
-        int maxCoversation = 8;
-        string[] characterSetting = { "ギャル", "おじいちゃん" };
-
+        // チャットクライアント作成
         await chatClient1.GetCompletionAsync($"あなたは{characterSetting[0]}です。その口調で話してください。話し相手は{characterSetting[1]}です。そのつもりで話してください。", (_) => { });
         await chatClient2.GetCompletionAsync($"あなたは{characterSetting[1]}です。その口調で話してください。話し相手は{characterSetting[0]}です。そのつもりで話してください。", (_) => { });
 
-        string aitenoResponse = "";
+        var aitenoResponse = "";
 
-        // その後は、お互いのレスポンスを入力として、会話を続ける。
-        for (int i = 0; i < maxCoversation; i++)
+        // チャット処理
+        for (int i = 0; i < (maxCoversation * 2); i++)
         {
             // 最初の一回は、ユーザーが入れたプロンプトをもとに回答し、それ以降は相手の回答をもとに回答する。
-            var prompt = (aitenoResponse == "") ? userPrompt : aitenoResponse;
+            var prompt = prerequisites + ((aitenoResponse == "") ? userPrompt : aitenoResponse);
             // 相手の回答をもとにこの回のinputを作ったら、前回の相手の回答はリセットする
             aitenoResponse = "";
             var name = characterSetting[i % 2];
-            ResponseBlock1.Text += $"■{name} の発言\r\n";
+            ResponseBlock1.Text += $"■{name} の発言({(i / 2) + 1})\r\n";
 
             // 回答を生成
             await GetTargetClient(i)!.GetCompletionAsync(prompt, (response =>
             {
+                // 一文字ずつ画面に表示
                 ResponseBlock1.Text += response;
                 aitenoResponse += response;
             }));
+
+            // 最後に改行を入れる
             ResponseBlock1.Text += "\r\n\r\n";
+
             await Task.Delay(1000);
         }
 
-        // 指定の回数の会話が終わったら、入力欄をリセットして送信ボタンを有効化する
-        PromptBox.Text = "";
+        // 指定の回数の会話が終わったら、送信ボタンを有効化する
         SendButton.IsEnabled = true;
 
-        MyChatClient? GetTargetClient(int count)
-        {
-            return ((count % 2) == 0) ? chatClient1 : chatClient2;
-        }
+        // 一回の会話が終わったら、クライアントを破棄する
+        chatClient1?.Dispose();
+        chatClient2?.Dispose();
     }
 
-    private async void Window_Closed(object sender, EventArgs e)
+    private void Window_Closed(object sender, EventArgs e)
     {
         chatClient1?.Dispose();
         chatClient2?.Dispose();
+    }
+
+    private MyChatClient? GetTargetClient(int count)
+    {
+        return ((count % 2) == 0) ? chatClient1 : chatClient2;
     }
 }
